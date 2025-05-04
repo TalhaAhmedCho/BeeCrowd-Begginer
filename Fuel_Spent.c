e574c67013e077b16264c5fa0c11ac0f330ab21e
TalhaAhmedCho <tahchma@gmail.com>
2025-05-04 17:57:10 +0600

    1017

---
+++ Fuel_Spent.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+  int time, speed;
+  float distance, fuelSpent;
+
+  scanf("%d", &time);
+  scanf("%d", &speed);
+  distance = time * speed;
+  fuelSpent = distance / 12.0;
+  
+  printf("%.3f\n", fuelSpent);
+
+ 
+  return 0;
+}
