9bacd2f169f85458dbff71071cc2edca48419ce3
TalhaAhmedCho <tahchma@gmail.com>
2025-05-05 20:47:03 +0600

    1020

---
+++ Age_in_Days.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+  int days, years, months;
+  scanf("%d", &days);
+
+  years = days / 365;
+  months = (days % 365) / 30;
+  days = (days % 365) % 30;
+
+  printf("%d ano(s)\n", years);
+  printf("%d mes(es)\n", months);
+  printf("%d dia(s)\n", days);
+ 
+  return 0;
+}
