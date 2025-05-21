777674da456514532b60cfa041d9bd70cb4bfe44
TalhaAhmedCho <tahchma@gmail.com>
2025-05-21 21:59:27 +0600

    1070

---
+++ Six_Odd_Numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+  int i, n;
+  scanf("%d", &n);
+
+  for (i = 0; i < 6; i++)
+  {
+    if (n % 2 == 0)
+    {
+      n++;
+    }
+    printf("%d\n", n);
+    n += 2;
+  }
+  
+  return 0;
+}
